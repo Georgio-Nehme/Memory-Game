4891b700bb66c22c4de79f113e563454d57e1ebd
GeorgioNehme <georgionehme@gmail.com>
2022-05-23 00:41:01 +0300

    Syntax correction and small modifications  (still not functional)

--- memory_game.js
+++ memory_game.js
@@ -10,38 +10,57 @@ const red_btn = document.getElementById("top_right");
 const blue_btn = document.getElementById("bottom_left");
 const yellow_btn = document.getElementById("bottom_right");
 
-document.getElementById("top_left").onclick = function() {press_green()}
-document.getElementById("top_right").onclick = function() {press_red()}
-document.getElementById("bottom_left").onclick = function() {press_blue()}
-document.getElementById("bottom_right").onclick = function() {press_yellow()}
+//Event listeners//
+document.getElementById("top_left").onclick = function() {press_green();}
+document.getElementById("top_right").onclick = function() {press_red();}
+document.getElementById("bottom_left").onclick = function() {press_blue();}
+document.getElementById("bottom_right").onclick = function() {press_yellow();}
 
 //Assigning the variables//
 let player_sequence = [];
-// let temp_player_sequence = [];
 let rand_sequence = [];
 const colors = ["green", "red", "blue", "yellow"];
-var color = " "
-
+var color = " ";
+green = "green";
+red = "red";
+blue = "blue";
+yellow = "yellow";
 
 ////////////////
   //Game loader//
 ///////////////
 document.addEventListener("keypress", function(event) {
-let level = 1; //Dummy Value//
 
-start_game(level)
-console.log(rand_sequence)
+start_game(1)
+    
+
+
+
 });
 
+
+
+//////////////
+  //Functions//
+/////////////
+
+
+
+// under construction //\\
+                     //  \\
+                    // !! \\ 
+                   //  !!  \\
+                  //   ..   \\
+                 //==========\\
 function start_game (level)  {
     
     while (level !== 0) {
         add_random(colors, rand_sequence); //adding a color to the random sequence//
-        green_btn.addEventListener("click", press_green(player_sequence));
-        blue_btn.addEventListener("click", press_blue(player_sequence));
-        red_btn.addEventListener("click", press_red(player_sequence));
-        yellow_btn.addEventListener("click", press_yellow(player_sequence));
-        
+        // green_btn.addEventListener("click", press_green(player_sequence));
+        // blue_btn.addEventListener("click", press_blue(player_sequence));
+        // red_btn.addEventListener("click", press_red(player_sequence));
+        // yellow_btn.addEventListener("click", press_yellow(player_sequence));
+                
         if (last_value(player_sequence) !== last_value(rand_sequence)){
             press_wrong();
             let level = 0; //You lost case//
@@ -54,21 +73,6 @@ function start_game (level)  {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////
-  //Functions//
-/////////////
 
 
 //Function to add random values/
@@ -87,10 +91,10 @@ function last_value(array) {
 //Random sequence player//
 function play_new_color(array) {
     let x =last_value(array);
-    if      (x==="red")    {press_red(rand_sequence)}
-    else if (x === "blue") {press_blue(rand_sequence)}
-    else if (x === "green"){press_green(rand_sequence)}
-    else                   {press_yellow(rand_sequence)}
+    if      (x==="red")    {press_red(rand_sequence);}
+    else if (x === "blue") {press_blue(rand_sequence);}
+    else if (x === "green"){press_green(rand_sequence);}
+    else                   {press_yellow(rand_sequence);}
 }    
 
 
@@ -100,26 +104,26 @@ function press_green(array) {
     green_sound.play();
     green_btn.style.backgroundColor = "grey";
     setTimeout(() => {green_btn.style.backgroundColor = "green";}, 200);
-    array.push("green");
-};
+    array.push(green);
+}
 function press_red(array) {
     red_sound.play();
     red_btn.style.backgroundColor = "grey";
     setTimeout(() => {red_btn.style.backgroundColor = "red";}, 200);
-    array.push("red");
-};
+    array.push(red);
+}
 function press_blue(array) {
     blue_sound.play();
     blue_btn.style.backgroundColor = "grey";
     setTimeout(() => {blue_btn.style.backgroundColor = "blue";}, 200);
-    array.push("blue");
-};
+    array.push(blue);
+}
 function press_yellow(array) {
     yellow_sound.play();
     yellow_btn.style.backgroundColor = "grey";
     setTimeout(() => {yellow_btn.style.backgroundColor = "yellow";}, 200);
-    array.push("yellow");
-};
+    array.push(yellow);
+}
 
 function press_wrong() {
     wrong_sound.play();
