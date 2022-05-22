d552ef5bc491fba70e3ab78e65061a193892c699
GeorgioNehme <georgionehme@gmail.com>
2022-05-22 15:07:17 +0300

    Made some modifications but still not working

--- memory_game.js
+++ memory_game.js
@@ -1,4 +1,4 @@
-    // Audiovisual Effects //
+// Audiovisual Effects //
 const green_sound = new Audio("assets/sounds/green.mp3");
 const red_sound = new Audio("assets/sounds/red.mp3");
 const blue_sound = new Audio("assets/sounds/blue.mp3");
@@ -15,12 +15,42 @@ document.getElementById("top_right").onclick = function() {press_red()}
 document.getElementById("bottom_left").onclick = function() {press_blue()}
 document.getElementById("bottom_right").onclick = function() {press_yellow()}
 
-
-var level = 0;
-var player_sequence = [];
-var temp_player_sequence = [];
-var rand_sequence = [];
+//Assigning the variables//
+let player_sequence = [];
+// let temp_player_sequence = [];
+let rand_sequence = [];
 const colors = ["green", "red", "blue", "yellow"];
+var color = " "
+
+
+////////////////
+  //Game loader//
+///////////////
+document.addEventListener("keypress", function(event) {
+let level = 1; //Dummy Value//
+
+start_game(level)
+console.log(rand_sequence)
+});
+
+function start_game (level)  {
+    
+    while (level !== 0) {
+        add_random(colors, rand_sequence); //adding a color to the random sequence//
+        green_btn.addEventListener("click", press_green(player_sequence));
+        blue_btn.addEventListener("click", press_blue(player_sequence));
+        red_btn.addEventListener("click", press_red(player_sequence));
+        yellow_btn.addEventListener("click", press_yellow(player_sequence));
+        
+        if (last_value(player_sequence) !== last_value(rand_sequence)){
+            press_wrong();
+            let level = 0; //You lost case//
+        } else{
+            level++; //You passed case//
+            start_game(level);
+        } 
+    }
+}
 
 
 
@@ -36,66 +66,63 @@ const colors = ["green", "red", "blue", "yellow"];
 
 
 
+//////////////
+  //Functions//
+/////////////
+
+
+//Function to add random values/
+function add_random(array, rand_array) {
+    var temp_value = array[Math.floor(Math.random() * array.length)];
+    rand_array.push(temp_value);
+    return rand_array;
+}
+//Function to return the last value recorded in an array//
+function last_value(array) {
+    return array[array.length -1];
+}
 
 
 
+//Random sequence player//
+function play_new_color(array) {
+    let x =last_value(array);
+    if      (x==="red")    {press_red(rand_sequence)}
+    else if (x === "blue") {press_blue(rand_sequence)}
+    else if (x === "green"){press_green(rand_sequence)}
+    else                   {press_yellow(rand_sequence)}
+}    
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function press_green() {
+//Functions for the buttons animation//
+function press_green(array) {
     green_sound.play();
     green_btn.style.backgroundColor = "grey";
-    setTimeout(() => {green_btn.style.backgroundColor = "green";}, 100);
+    setTimeout(() => {green_btn.style.backgroundColor = "green";}, 200);
+    array.push("green");
 };
-
-function press_red() {
+function press_red(array) {
     red_sound.play();
     red_btn.style.backgroundColor = "grey";
-    setTimeout(() => {red_btn.style.backgroundColor = "red";}, 100);
+    setTimeout(() => {red_btn.style.backgroundColor = "red";}, 200);
+    array.push("red");
 };
-
-function press_blue() {
+function press_blue(array) {
     blue_sound.play();
     blue_btn.style.backgroundColor = "grey";
-    setTimeout(() => {blue_btn.style.backgroundColor = "blue";}, 100);
+    setTimeout(() => {blue_btn.style.backgroundColor = "blue";}, 200);
+    array.push("blue");
 };
-
-function press_yellow() {
+function press_yellow(array) {
     yellow_sound.play();
     yellow_btn.style.backgroundColor = "grey";
-    setTimeout(() => {yellow_btn.style.backgroundColor = "yellow";}, 100);
+    setTimeout(() => {yellow_btn.style.backgroundColor = "yellow";}, 200);
+    array.push("yellow");
 };
 
 function press_wrong() {
     wrong_sound.play();
     game_container.style.backgroundColor = "red";
-    setTimeout(() => {game_container.style.backgroundColor = "df7777";}, 100);
-};
+    setTimeout(() => {game_container.style.backgroundColor = "df7777";}, 200);
+}
